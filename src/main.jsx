75bee1d6a5744a215f92f2f25ff98126c309be2c
uzair23454-debug <muzairshafieq@gmail.com>
2025-12-28 20:25:31 +0500

    Fix: Remove duplicate App import causing build fail

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
 import { StoreProvider } from './contexts/StoreContext';
 import { ToastProvider } from './contexts/ToastContext';
 import App from './App';
